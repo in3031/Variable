704f9f8f12d4f1cbb9e3b6357b6d703fbd5a7b8c
in3031 <dogs4life>
2021-03-19 00:04:03 +0300

    2

--- src/ru/dd/in3031/first/Variable.java
+++ src/ru/dd/in3031/first/Variable.java
@@ -2,15 +2,25 @@ package ru.dd.in3031.first;
 
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Variable {
     public static void main(String[] args) {
-
-
         Scanner sc = new Scanner(System.in);
         String cnt = sc.next();
-        Pattern pattern=Pattern.compile(cnt);
-        System.out.println(cnt);
+
+
+        char[] strToArray = cnt.toCharArray();
+        for (int i = 0; i < strToArray.length; i++) {
+            strToArray.equals(cnt);
+            System.out.print(cnt);
+        }
+
+//        Pattern pattern = Pattern.compile("t(.)");
+//        Matcher matcher = pattern.matcher(cnt);
+//        while (matcher.find()) {
+//            System.out.println(matcher.group());
+//        }
     }
 }
